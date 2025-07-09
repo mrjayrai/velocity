da261790c93a850f70b2d2756af4be2121d104dd
Daksh Yadav <2000140896@hexaware.com>
2025-07-09 11:46:34 +0530

    Flask Server

---
+++ flask-backend/src/app/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+# from routes import resume_agent_routes, attendance_agent_routes,opportunity_agent_routes,training_agent_routes
+
+def create_app():  
+    app = Flask(__name__)
+    
+    # Register blueprints
+    # app.register_blueprint(resume_agent_routes.bp)
+    # app.register_blueprint(attendance_agent_routes.bp)
+    # app.register_blueprint(opportunity_agent_routes.bp)
+    # app.register_blueprint(training_agent_routes.bp)
+    
+    return app
