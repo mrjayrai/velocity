7dbe1c112a5d1c8e43ca7a10050ef62de664e3dd
mrjayrai <raijai127@gmail.com>
2025-07-17 04:16:16 +0530

    Changed Port in flask

--- flask-backend/src/run.py
+++ flask-backend/src/run.py
@@ -2,5 +2,10 @@ from app import create_app
 
 app= create_app()
 
+# if __name__ == "__main__":
+#     app.run(debug=True,port=5000)
+
 if __name__ == "__main__":
-    app.run(debug=True,port=5000)
+    import os
+    # port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=5000)
